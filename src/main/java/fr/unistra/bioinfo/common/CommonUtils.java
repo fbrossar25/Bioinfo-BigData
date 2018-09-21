39a543b52e85f19073d3589d5aca52d53a7ce9ef
Unknown <florian.brossard@edu.univ-fcomte.fr>
2018-09-21 15:54:19 +0200

    Liste des di-trinucleotides

---
+++ src/main/java/fr/unistra/bioinfo/common/CommonUtils.java
@@ -0,0 +1,33 @@
+package fr.unistra.bioinfo.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CommonUtils {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final Set<String> DINUCLEOTIDES;
+    public static final Set<String> TRINUCLEOTIDES;
+    static{
+        Set<String> diset = new TreeSet<>();
+        Set<String> triset = new TreeSet<>();
+        String map = "ACGT";
+        for ( char i = 0 ; i < map.length() ; i++ ) {
+            for ( char j = 0 ; j < map.length() ; j++ ) {
+                diset.add("" + map.charAt(i) + map.charAt(j));
+                for ( char k = 0 ; k < map.length() ; k++ ) {
+                    triset.add("" + map.charAt(i) + map.charAt(j) + map.charAt(k));
+                }
+            }
+        }
+        DINUCLEOTIDES = Collections.unmodifiableSet(diset);
+        TRINUCLEOTIDES = Collections.unmodifiableSet(triset);
+        LOGGER.debug("DINUCLEOTIDES : " + Arrays.toString(DINUCLEOTIDES.toArray()));
+        LOGGER.debug("TRINUCLEOTIDES : " + Arrays.toString(TRINUCLEOTIDES.toArray()));
+    }
+}
