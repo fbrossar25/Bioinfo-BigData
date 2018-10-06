b8d28a120bffb0fa73f1090b860f3fdc7d82bb2b
Janowskyj Thomas <tjanowskyj@gmail.com>
2018-10-06 17:27:28 +0200

    Ajout BorderPane + Logs dans la fenêtre + action bouton

--- src/main/java/fr/unistra/bioinfo/gui/MainWindowController.java
+++ src/main/java/fr/unistra/bioinfo/gui/MainWindowController.java
@@ -2,18 +2,24 @@ package fr.unistra.bioinfo.gui;
 
 import fr.unistra.bioinfo.Main;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 
 public class MainWindowController {
     public BorderPane panelPrincipal;
     public MenuBar barreMenu;
     public Menu menuFichier;
+    public Button btnMain;
     public MenuItem btnNettoyerDonnees;
     public MenuItem btnSupprimerFichiersGEnomes;
     public MenuItem btnQuitter;
+    public TextArea logs;
+
+
+    public void demarrer(ActionEvent actionEvent){
+        TextAreaAppender.setTa(logs);
+        Main.generateOrganismDirectories();
+    }
 
     public void nettoyerDonnees(ActionEvent actionEvent) {
     }
