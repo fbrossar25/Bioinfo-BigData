3d93177691a38f8e673ef621a1684c6e789d33e1
brossard <florian.brossard@etu.unistra.fr>
2019-04-18 00:42:53 +0200

    Lecture fichier OK

--- src/main/java/fr/unistra/bioinfo/parsing/GenbankReader.java
+++ src/main/java/fr/unistra/bioinfo/parsing/GenbankReader.java
@@ -3,47 +3,44 @@ package fr.unistra.bioinfo.parsing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GenbankReader {
-    enum Operator{
-        JOIN, COMPLEMENT, COMPLEMENT_JOIN, NONE
-    }
-
-    class CDS{
-        int begin;
-        int end;
-        Operator op;
-    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GenbankReader.class);
-    private File file;
-    private BufferedReader buffer;
-    private StringBuilder origin = new StringBuilder();
-    private List<CDS> cdsList = new ArrayList<>();
-    private List<StringBuilder> processedCdsList = new ArrayList<>();
+    private final File file;
+    private final StringBuilder buffer;
+    private final StringBuilder origin = new StringBuilder();
+    private final List<StringBuilder> cdsList = new ArrayList<>();
+    private final List<StringBuilder> subsequences = new ArrayList<>();
 
     private GenbankReader(File file) throws IOException {
         this.file = file;
-        buffer = new BufferedReader(new FileReader(file));
+        buffer = new StringBuilder((int)file.length());
     }
 
     private void process() throws IOException{
         String line;
-        while((line = buffer.readLine()) != null){
-            processLine(line);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        while((line = reader.readLine()) != null){
+            buffer.append(line);
         }
-    }
-
-    private void processLine(String line){
+        try{
+            reader.close();
+        }catch (IOException e){
+            //ignore
+        }
         //TODO
     }
 
     public static List<StringBuilder> parse(File file) throws IOException{
         GenbankReader reader = new GenbankReader(file);
         reader.process();
-        return reader.processedCdsList;
+        return reader.subsequences;
     }
 }
