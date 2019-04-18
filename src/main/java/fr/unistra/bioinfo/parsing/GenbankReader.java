df5bf6eda37e49d80f8eb0273ffe2632d78d3389
brossard <florian.brossard@etu.unistra.fr>
2019-04-18 12:50:44 +0200

    Parsing VERSION + TU GenbankReader

--- src/main/java/fr/unistra/bioinfo/parsing/GenbankReader.java
+++ src/main/java/fr/unistra/bioinfo/parsing/GenbankReader.java
@@ -2,12 +2,18 @@ package fr.unistra.bioinfo.parsing;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GenbankReader {
+    private static final Pattern SECTION_PATTERN = Pattern.compile("^\\s*(.+?)(\\s+(.+))?$");
+    private static final Pattern VERSION_PATTERN = Pattern.compile("^(NC_\\d+?).(\\d+)$");
+
     enum Operator{
         JOIN, COMPLEMENT, COMPLEMENT_JOIN, NONE
     }
@@ -20,30 +26,92 @@ public class GenbankReader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GenbankReader.class);
     private File file;
-    private BufferedReader buffer;
+    private BufferedReader reader;
     private StringBuilder origin = new StringBuilder();
     private List<CDS> cdsList = new ArrayList<>();
     private List<StringBuilder> processedCdsList = new ArrayList<>();
+    private String name = null;
+    private int version = 0;
 
-    private GenbankReader(File file) throws IOException {
+    GenbankReader(@NonNull File file){
         this.file = file;
-        buffer = new BufferedReader(new FileReader(file));
     }
 
-    private void process() throws IOException{
+    private void process() throws IOException {
+        reader = new BufferedReader(new FileReader(file));
         String line;
-        while((line = buffer.readLine()) != null){
+        while((line = reader.readLine()) != null){
             processLine(line);
         }
     }
 
     private void processLine(String line){
+        Matcher sectionMatcher = SECTION_PATTERN.matcher(line);
+        String sectionName;
+        String sectionLine = null;
+        if(sectionMatcher.matches()){
+            sectionName = sectionMatcher.group(1);
+            if(sectionMatcher.groupCount() == 3){
+                sectionLine = sectionMatcher.group(3);
+            }
+            processSection(sectionName, sectionLine);
+        }
+    }
+
+    private void processSection(String name, String value){
+        switch (name.toUpperCase()){
+            case "VERSION":
+                processVersion(value);
+                break;
+            case "CDS":
+                processCDS(value);
+                break;
+            case "ORIGIN":
+                processORIGIN();
+                break;
+            default:
+        }
+    }
+
+    private void processVersion(String versionValue){
+        Matcher versionMatcher = VERSION_PATTERN.matcher(versionValue);
+        if(versionMatcher.matches()){
+            name = versionMatcher.group(1);
+            version = Integer.parseInt(versionMatcher.group(2));
+        }
+    }
+
+    private void processCDS(String cdsValue){
         //TODO
     }
 
-    public static List<StringBuilder> parse(File file) throws IOException{
-        GenbankReader reader = new GenbankReader(file);
-        reader.process();
-        return reader.processedCdsList;
+    private void processORIGIN(){
+        //TODO
+    }
+
+    public List<StringBuilder> getProcessedCdsList(){
+        return processedCdsList;
+    }
+
+    public int getValidsCDS(){
+        return -1;
+    }
+
+    public int getInvalidsCDS(){
+        return -1;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getVersion(){
+        return version;
+    }
+
+    public static GenbankReader parse(File file) throws IOException{
+        GenbankReader genbankReader = new GenbankReader(file);
+        genbankReader.process();
+        return genbankReader;
     }
 }
---
+++ src/test/java/fr/unistra/bioinfo/parsing/GenbankReaderTest.java
@@ -0,0 +1,45 @@
+package fr.unistra.bioinfo.parsing;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenbankReaderTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenbankReaderTest.class);
+
+    private static final Path TEST_FILE = Paths.get("src", "test", "resources", "complement-test.gb");
+
+    @Test
+    void shouldParseVersion(){
+        try {
+            GenbankReader gbReader = GenbankReader.parse(TEST_FILE.toFile());
+            assertEquals("NC_001700", gbReader.getName());
+            assertEquals(1, gbReader.getVersion());
+        } catch (IOException e) {
+            fail("Erreur lors de la lecture du fichier",e);
+        }
+    }
+
+
+    @Test
+    void shouldParseCDS(){
+        try {
+            GenbankReader gbReader = GenbankReader.parse(TEST_FILE.toFile());
+            assertEquals(2, gbReader.getValidsCDS());
+            assertEquals(0, gbReader.getInvalidsCDS());
+            List<StringBuilder> cdsList = gbReader.getProcessedCdsList();
+            assertNotNull(cdsList);
+            assertEquals(2, cdsList.size());
+            assertEquals(cdsList.get(0), cdsList.get(1)); //le CDS 2 est le complement du CDS 1
+        } catch (IOException e) {
+            fail("Erreur lors de la lecture du fichier",e);
+        }
+    }
+}
